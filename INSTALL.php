93fdf9b90c66f0bab7daf9c4e1b08aea0ead8dda
Malte Kiefer <malte.kiefer@mailgermania.de>
2015-08-24 14:29:21 +0200

    Update INSTALL.php

--- INSTALL.php
+++ INSTALL.php
@@ -5,7 +5,6 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
     <title>Install ShoppingList</title>
   </head>
   <body>
