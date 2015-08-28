1dc72a2a6e46e8225fbf3e8872f0809a5f861d58
Jan Buchta <J-8@users.noreply.github.com>
2015-08-28 08:55:13 +0200

    Create CONSTANTS.php
    Used for improved error handling.

---
+++ CONSTANTS.php
@@ -0,0 +1,24 @@
+<?php
+	define('API_SUCCESS_LIST', 1000);
+	define('API_SUCCESS_LIST_EMPTY', 1001);
+    define('API_SUCCESS_UPDATE', 1002);
+    define('API_SUCCESS_FAVORITE', 1003);
+    define('API_SUCCESS_DELETE', 1004);
+    define('API_SUCCESS_SAVE', 1005);
+	define('API_SUCCESS_CLEAR', 1006);
+	
+	define('API_ERROR_SERVER', 5000);
+    define('API_ERROR_404', 5001);
+    define('API_ERROR_403', 5002);
+    define('API_ERROR_MISSING_FUNCTION', 5003);
+    define('API_ERROR_NO_DATABASE', 5004);
+    define('API_ERROR_CONFIG', 5005);
+    define('API_ERROR_UNKNOWN', 5006);
+    define('API_ERROR_UPDATE_', 5007);
+    define('API_ERROR_FAVORITE', 5008);
+    define('API_ERROR_DELETE', 5009);
+    define('API_ERROR_SAVE', 5010);
+	define('API_ERROR_CLEAR', 5011);
+	define('API_ERROR_DATABASE_CONNECT', 5012);
+	define('API_ERROR_MISSING_PARAMETER', 5013);
+?>
