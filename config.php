9ff5becf05307c8ef239e04f49ef05dea566723e
Malte Kiefer <malte.kiefer@mailgermania.de>
2015-08-31 15:27:49 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,25 +1,28 @@
 <?php
-    
-    //hashed password used for app
-    //to get a hash of your password, run 'php config.php <password>' on the command line.
-    //It is important to put the hashed password into single quotes ''!
-	$authKey = '';
+	/*##########################################################
+	# Last Author: beli3ver
+	# Last edit: 2015-08-31 03:30pm
+	############################################################*/
 	
+	/*##########################################################
+	# Auth Key / Password, to get acces to the database	   #
+	############################################################*/
+    	$authKey = '';
+	/*##########################################################
+	# Branch: stable, testing, experimental			   #
+	############################################################*/	
+	$branch = "stable"
+	/*##########################################################
+	# Database: MySQL, SQLite				   #
+	############################################################*/	
 	$dataBase = "SQLite";
-	//only for SQLite
-	$SQLiteConfig = [
-        'file' => "shoppinglist.sqlite",
-	];
-	//only for MySQL
-	$MySQLConfig = [
-        'host' => "host",
-        'db' => "db",
-        'user' => "user",
-        'password' => "password",
-    ];
-    
-    if($argc > 1){
-		echo password_hash($argv[1], PASSWORD_BCRYPT);
-	}
+	/*##########################################################
+	# SQLite Config						   #
+	############################################################*/		
+	$SQLiteConfig = ['file' => "shoppinglist.sqlite"];
+	/*##########################################################
+	# MySQL Config						   #
+	############################################################*/
+	$MySQLConfig = ['host' => "host",'db' => "db",'user' => "user",'password' => "password",];
 ?>
 
