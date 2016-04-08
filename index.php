ac5ef9d33553fbf7023bbdc3539db584a331c5a9
the0ne <github.com@t-horner.com>
2016-04-08 20:12:12 +0200

    added viewport directive to support mobile device browsers

--- index.php
+++ index.php
@@ -33,6 +33,7 @@
 <html>
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Shopping List</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
